df0c812d4fb03f5c9f05a8e366e033bf257d5827
Martin Gebiertas <martingebiertas101@gmail.com>
2024-10-17 21:42:12 +0700

    new

--- app/src/main/java/com/example/myappkotlin/DistanceActivity.kt
+++ app/src/main/java/com/example/myappkotlin/DistanceActivity.kt
@@ -7,7 +7,6 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
-import android.util.Log
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
