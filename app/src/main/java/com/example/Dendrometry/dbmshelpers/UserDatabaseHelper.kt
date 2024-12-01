d46bee020b9b6110e3944576e653aa9e467fb3b6
Martin Gebiertas <martingebiertas101@gmail.com>
2024-12-02 00:10:01 +0700

    camera

--- app/src/main/java/com/example/Dendrometry/dbmshelpers/UserDatabaseHelper.kt
+++ app/src/main/java/com/example/Dendrometry/dbmshelpers/UserDatabaseHelper.kt
@@ -1,4 +1,26 @@
 package com.example.Dendrometry.dbmshelpers
 
-class UserDatabaseHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class UserDatabaseHelper (context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
+
+    companion object{
+        private const val DATABASE_NAME = "UserDatabase.db"
+        private const val DATABASE_VERSION = 1
+        private const val TABLE_NAME = "users"
+        private const val COLUMN_ID = "id"
+        private const val COLUMN_NAME = "name"
+        private const val COLUMN_USERNAME = "username"
+        private const val COLUMN_PASSWORD = "password"
+    }
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+
+    }
 }
